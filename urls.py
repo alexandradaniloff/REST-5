fa8b6d7a32a22401dcc62315df1da8e8e599e74e
Alexander <alexandradaniloff@gmail.ru>
2024-06-16 13:25:45 +0300

    добавил yasg/py

---
+++ urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for pereval project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path
+from django.contrib import admin
+from rest_framework import routers
+from project import views
+from project.yasg import urlpatterns as doc_urls
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UsersViewset, basename='users')
+router.register(r'coords', views.CoordsViewset, basename='coords')
+router.register(r'levels', views.LevelViewset, basename='levels')
+router.register(r'images', views.ImagesViewset, basename='images')
+router.register(r'perevals', views.PerevalViewset, basename='perevals')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
+urlpatterns += doc_urls
